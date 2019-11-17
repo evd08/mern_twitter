171dc2fe8252c324ece7c835cc45d5ed9d2d0b92
eunice <eunice.dayrit09@gmail.com>
2019-11-16 19:05:51 -0800

    first commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb://dev:yt3Ana1VFETdMbTF@cluster.mongodb.net/test?retryWrites=true&w=majority"
+    // "mongodb+srv://dev:yt3Ana1VFETdMbTF@cluster0-mdqrv.mongodb.net/test?retryWrites=true&w=majority"
+  //Make sure this is your own unique string
+};
